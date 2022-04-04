7c2deaca545fa9249d66da3f940c2b66941986cd
mahmudul1995 <mahmuduls451@gmail.com>
2022-04-05 00:44:56 +0600

    dashboard complete

--- src/Components/Dashboards/Dashboards.js
+++ src/Components/Dashboards/Dashboards.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Bar, BarChart, CartesianGrid, Tooltip, Legend, XAxis, YAxis, PieChart, Pie } from 'recharts';
-import PieeChart from './PieeChart';
-
-
+import { Bar, BarChart, CartesianGrid, Tooltip, Legend, XAxis, YAxis, PieChart, Pie, Line, LineChart, Area, AreaChart } from 'recharts';
 
 const Dashboards = () => {
     const data = [
@@ -45,8 +42,70 @@ const Dashboards = () => {
     ];
 
     return (
-        <div className=" grid grid-cols-12 ">
-            <div className="first-chart">
+        <div className="grid grid-cols-1  md:grid-cols-2 ">
+
+            <div className="first-chart mt-5">
+                <h2 className="text-blue-500">Investment Vs Revenue</h2>
+                <AreaChart
+                    width={500}
+                    height={280}
+                    data={data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="revenue" stroke="#8884d8" fill="#8884d8" />
+                </AreaChart>
+            </div>
+
+            <div className="second-chart mt-5">
+                <h3 className="text-blue-500">Month Wise Sell</h3>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                        type="monotone"
+                        dataKey="sell"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                    />
+
+                </LineChart>
+            </div>
+
+
+            <div className="third-chart mt-5">
+                {/* <PieeChart></PieeChart> */}
+                    <h2 className="text-blue-500">Invest Vs Revenue</h2>
+                <PieChart width={400} height={400}>
+                    <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                    <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                </PieChart>
+
+            </div>
+
+            <div className="forth-chart mt-5">
+            <h2 className="text-blue-500">Invest Vs Revenue</h2>
                 <BarChart
                     width={500}
                     height={300}
@@ -66,18 +125,6 @@ const Dashboards = () => {
                     <Bar dataKey="investment" stackId="a" fill="#8884d8" />
                     <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
                 </BarChart>
-            </div>
-            <div className="second-chart">
-                {/* <PieeChart></PieeChart> */}
-
-                <PieChart width={400} height={400}>
-                    <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
-                    <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-                </PieChart>
-
-            </div>
-            <div className="second-chart">
-
             </div>
         </div>
     );
